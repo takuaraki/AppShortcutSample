b0e3d2d6f56f832e874391a9dc7f1b5234001268
takuaraki <takuaraki7@gmail.com>
2016-11-28 17:46:09 +0900

    add MainActivity#createIntent

--- app/src/main/java/com/example/arakitaku/appshortcutsample/MainActivity.java
+++ app/src/main/java/com/example/arakitaku/appshortcutsample/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.arakitaku.appshortcutsample;
 
+import android.content.Context;
 import android.content.Intent;
 import android.content.pm.ShortcutInfo;
 import android.content.pm.ShortcutManager;
@@ -18,6 +19,16 @@ import java.util.Arrays;
 import java.util.Collections;
 
 public class MainActivity extends AppCompatActivity {
+
+    /**
+     * Create Intent for launch this activity.
+     *
+     * @param context Context
+     * @return Intent for launch this activity
+     */
+    public static Intent createIntent(Context context) {
+        return new Intent(context, MainActivity.class);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -83,27 +94,5 @@ public class MainActivity extends AppCompatActivity {
             }
         }
 
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 }
